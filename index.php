1749d8e4d5a179e3b830421e4f3a0a2f9e949e6b
dragondungeon1 <lennartderidder@gmail.com>
2020-04-27 14:31:11 +0200

    first init

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="js/bootstrap.bundle.js">
+    <link rel="stylesheet" href="js/bootstrap.js">
+    <link rel="stylesheet" href="css/own-css.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+       <ul class=" nav nav-tabs justify-content-around " role="tablist ">
+           <h3>Portfolio Lennart de Ridder</h3>
+
+</div>
+<br>
+<div class="container">
+    <img class="own1 img-fluid rounded-circle " src="img/grey.png" alt="photo of me :D">
+</div>
+<br>
+<div class="container">
+    <section class="">
+        <h2>Beste lezer..</h2>
+
+        <blockquote class="blockquote well ">
+             Mijn naam is Lennart de Ridder, ik ben 18 jaar oud en zit op het MBORijnland in Zoetermeer. <br>
+             Ik zit in mijn eerste jaar van de opleiding: Applicatie en mediaontwikkelaar. Fijn dat u mijn <br>
+            portfolio komt bekijken, ik neem u mee door een aantal punten die u waarschijnlijk wilt weten.
+        </blockquote>
+    </section>
+
+    <br>
+    <br>
+
+    <section class="text-right ">
+        <h2>Mijn hobby's</h2>
+
+        <blockquote class=" blockquote text-right  ">
+            Het liefst ben ik buiten als het mooi weer is zodat ik dan met vrienden een potje kan voetballen. <br>
+            Als ik niet naar buiten kan dan ben ik achter mijn computer te vinden zodat ik kan gamen  of <br>
+           programmeren, mijn favoriete games zijn: League of Legends, Call of duty modernwarfare 2 en battlefield 4. <br>
+        </blockquote>
+    </section>
+
+    <br>
+    <br>
+
+    <section>
+        <h2>Mijn vaardigheden</h2>
+
+        <table class="table table-bordered">
+            <thread>
+                <tr>
+                    <th>Vaardigheden</th>
+                    <th>Beheersing</th>
+                </tr>
+            </thread>
+            <tbody>
+                <tr class="table table-bordered">
+                    <td>PHP</td>
+                    <td class="progress progress-bar bg-warning progress-bar-animated progress-bar-striped" style="width: 30%;" >30%</td>
+                </tr>
+                <tr class="table table-bordered" >
+                    <td>HTML5</td>
+                    <td class="progress progress-bar bg-success progress-bar-animated progress-bar-striped" style="width: 70%;" >70%</td>
+                </tr>
+                <tr class="table table-bordered" >
+                    <td>CSS</td>
+                    <td class="progress progress-bar bg-success progress-bar-animated progress-bar-striped" style="width: 50%;" >50%</td>
+                </tr>
+                <tr class="table table-bordered" >
+                    <td>Bootstrap 4</td>
+                    <td class="progress progress-bar bg-warning progress-bar-animated progress-bar-striped" style="width: 20%;" >20%</td>
+                </tr>
+                <tr class="table table-bordered" >
+                    <td>JS</td>
+                    <td class="progress progress-bar bg-warning progress-bar-animated progress-bar-striped" style="width: 20%;" >20%</td>
+                </tr>
+                <tr class="table table-bordered  " >
+                    <td>Adobe photoshop</td>
+                    <td class="progress progress-bar  progress-bar-animated progress-bar-striped" style="width: 60%;" >60%</td>
+                </tr>
+                <tr class="table table-bordered" >
+                    <td>Adobe illustrator</td>
+                    <td class="progress progress-bar  progress-bar-animated progress-bar-striped" style="width: 50%;" >50%</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+    <br>
+    <br>
+
+    <section class="text-right">
+        <h2>Mijn verbeterpunten</h2>
+
+        <blockquote class="blockquote">
+                Zoals u hierboven ziet heb ik nog een paar punten die aan verbetering toe zijn, <br>
+                maar ik ben bezig met deze bij te werken zodat ik me op een breder gebied kan inzetten.
+        </blockquote>
+    </section>
+</div>
+
+</body>
+</html>
+
+<?php
