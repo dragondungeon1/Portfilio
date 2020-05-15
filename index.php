d61707db9dbc5cdd5f1d6220844ca927962f2bc3
dragondungeon1 <lennartderidder@gmail.com>
2020-05-15 15:46:19 +0200

    added a new navbar

--- index.php
+++ index.php
@@ -5,40 +5,66 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/bootstrap-grid.css">
-    <link rel="stylesheet" href="css/bootstrap-reboot.css">
-    <link rel="stylesheet" href="js/bootstrap.bundle.js">
-    <link rel="stylesheet" href="js/bootstrap.js">
     <link rel="stylesheet" href="css/own-css.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
-<div class="container">
-       <ul class=" nav nav-tabs justify-content-around " role="tablist ">
-           <h3>Portfolio Lennart de Ridder</h3>
+<div>
+    <div>
+        <nav class="menu">
+            <ul class="menu__list">
+                <img class="img-test" src="img/grey2.jpg" alt="logo">
+                <li class="menu__group"><a href="index.php" class="menu__link">Lennart de Ridder</a></li>
+                <li class="menu__group"><a href="index.php" class="menu__link">Home</a></li>
+                <li class="menu__group"><a href="#" class="menu__link">Rooms </a></li>
+                <li class="menu__group"><a href="accommodations.php" class="menu__link">Accommodations</a></li>
+                <li class="menu__group"><a href="#" class="menu__link">Locations</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+<div class="jumbotron" style="margin-bottom: 0px">
+    <h1>Portfolio</h1>
+    <p>Lennart de Ridder</p>
 
 </div>
+<!--<nav class="navbar navbar-expand-md list-unstyled navbar-dark bg-warning">-->
+<!--    <span class="navbar-text text-dark">Lennart de Ridder</span>-->
+<!--    <a class="navbar-brand " href="#"><img src="#" alt=""></a>-->
+<!---->
+<!---->
+<!--    <ul class="navbar-nav ">-->
+<!--    <li class="nav-item ">-->
+<!--        <a class=" nav-link text-dark " href="index.php">Home</a>-->
+<!--    </li>-->
+<!--    <li class="nav-item">-->
+<!--        <a class="nav-link text-dark" href="about.php">about</a>-->
+<!--    </li>-->
+<!--    <li class="nav-item">-->
+<!--        <a class="nav-link text-dark" href="contact.php">contact</a>-->
+<!--    </li>-->
+<!--    </ul>-->
+<!--</nav>-->
+
+
 <br>
-<div class="container">
-    <img class="own1 img-fluid rounded-circle " src="img/grey.png" alt="photo of me :D">
-</div>
-<br>
-<div class="container">
-    <section class="">
+<div class="container" >
+    <section class="" >
         <h2>Beste lezer..</h2>
-
-        <blockquote class="blockquote well ">
+        <blockquote class="blockquote well  ">
              Mijn naam is Lennart de Ridder, ik ben 18 jaar oud en zit op het MBORijnland in Zoetermeer. <br>
              Ik zit in mijn eerste jaar van de opleiding: Applicatie en mediaontwikkelaar. Fijn dat u mijn <br>
             portfolio komt bekijken, ik neem u mee door een aantal punten die u waarschijnlijk wilt weten.
         </blockquote>
     </section>
-
     <br>
     <br>
 
-    <section class="text-right ">
+    <section class="text-right  ">
         <h2>Mijn hobby's</h2>
 
         <blockquote class=" blockquote text-right  ">
@@ -50,54 +76,54 @@
 
     <br>
     <br>
-
-    <section>
-        <h2>Mijn vaardigheden</h2>
-
-        <table class="table table-bordered">
-            <thread>
-                <tr>
-                    <th>Vaardigheden</th>
-                    <th>Beheersing</th>
-                </tr>
-            </thread>
-            <tbody>
-                <tr class="table table-bordered">
-                    <td>PHP</td>
-                    <td class="progress progress-bar bg-warning progress-bar-animated progress-bar-striped" style="width: 30%;" >30%</td>
-                </tr>
-                <tr class="table table-bordered" >
-                    <td>HTML5</td>
-                    <td class="progress progress-bar bg-success progress-bar-animated progress-bar-striped" style="width: 70%;" >70%</td>
-                </tr>
-                <tr class="table table-bordered" >
-                    <td>CSS</td>
-                    <td class="progress progress-bar bg-success progress-bar-animated progress-bar-striped" style="width: 50%;" >50%</td>
-                </tr>
-                <tr class="table table-bordered" >
-                    <td>Bootstrap 4</td>
-                    <td class="progress progress-bar bg-danger progress-bar-animated progress-bar-striped" style="width: 20%;" >20%</td>
-                </tr>
-                <tr class="table table-bordered" >
-                    <td>JS</td>
-                    <td class="progress progress-bar bg-warning progress-bar-animated progress-bar-striped" style="width: 20%;" >20%</td>
-                </tr>
-                <tr class="table table-bordered  " >
-                    <td>Adobe photoshop</td>
-                    <td class="progress progress-bar  progress-bar-animated progress-bar-striped" style="width: 60%;" >60%</td>
-                </tr>
-                <tr class="table table-bordered" >
-                    <td>Adobe illustrator</td>
-                    <td class="progress progress-bar  progress-bar-animated progress-bar-striped" style="width: 50%;" >50%</td>
-                </tr>
-            </tbody>
-        </table>
-    </section>
+<!---->
+<!--    <section>-->
+<!--        <h2>Mijn vaardigheden</h2>-->
+<!---->
+<!--        <table class="table table-bordered">-->
+<!--            <thread>-->
+<!--                <tr>-->
+<!--                    <th>Vaardigheden</th>-->
+<!--                    <th>Beheersing</th>-->
+<!--                </tr>-->
+<!--            </thread>-->
+<!--            <tbody>-->
+<!--                <tr class="table table-bordered">-->
+<!--                    <td>PHP</td>-->
+<!--                    <td class="progress progress-bar bg-warning progress-bar-animated progress-bar-striped" style="width: 30%;" >30%</td>-->
+<!--                </tr>-->
+<!--                <tr class="table table-bordered" >-->
+<!--                    <td>HTML5</td>-->
+<!--                    <td class="progress progress-bar bg-success progress-bar-animated progress-bar-striped" style="width: 70%;" >70%</td>-->
+<!--                </tr>-->
+<!--                <tr class="table table-bordered" >-->
+<!--                    <td>CSS</td>-->
+<!--                    <td class="progress progress-bar bg-success progress-bar-animated progress-bar-striped" style="width: 50%;" >50%</td>-->
+<!--                </tr>-->
+<!--                <tr class="table table-bordered" >-->
+<!--                    <td>Bootstrap 4</td>-->
+<!--                    <td class="progress progress-bar bg-danger progress-bar-animated progress-bar-striped" style="width: 20%;" >20%</td>-->
+<!--                </tr>-->
+<!--                <tr class="table table-bordered" >-->
+<!--                    <td>JS</td>-->
+<!--                    <td class="progress progress-bar bg-warning progress-bar-animated progress-bar-striped" style="width: 20%;" >20%</td>-->
+<!--                </tr>-->
+<!--                <tr class="table table-bordered  " >-->
+<!--                    <td>Adobe photoshop</td>-->
+<!--                    <td class="progress progress-bar  progress-bar-animated progress-bar-striped" style="width: 60%;" >60%</td>-->
+<!--                </tr>-->
+<!--                <tr class="table table-bordered" >-->
+<!--                    <td>Adobe illustrator</td>-->
+<!--                    <td class="progress progress-bar  progress-bar-animated progress-bar-striped" style="width: 50%;" >50%</td>-->
+<!--                </tr>-->
+<!--            </tbody>-->
+<!--        </table>-->
+<!--    </section>-->
 
     <br>
     <br>
 
-    <section class="text-right">
+    <section >
         <h2>Mijn verbeterpunten</h2>
 
         <blockquote class="blockquote">
@@ -106,6 +132,8 @@
         </blockquote>
     </section>
 </div>
+
+
 
 </body>
 </html>
